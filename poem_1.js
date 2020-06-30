6bc09581f7d747174a1aafbddc5211c4111e35d3
Aneurinmind <50070328+Aneurinmind@users.noreply.github.com>
2020-07-01 00:02:53 +0800

    Add files via upload

---
+++ poem_1.js
@@ -0,0 +1,29 @@
+var obj_1 = document.getElementById("obj_1");
+
+//obj_1.onmouseover = function (){
+    //alert("鼠标已经移入！");
+	
+//}
+function PlaySound(bgm_1) {
+    var thissound=document.getElementById(bgm_1);
+    thissound.play();
+}
+
+function StopSound(bgm_1) {
+    var thissound=document.getElementById(bgm_1);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+var isPlaying =false; 
+	function readPlay(bgm_2){
+	var player = document.getElementById('bgm_2');
+	if (isPlaying) {
+	// 如果正在播放, 停止播放并停止读取此音乐文件
+	    player.pause();  
+	} else {
+	    player.play();
+	}
+	isPlaying = !isPlaying;
+	}
+
