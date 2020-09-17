e94d6a3ff42dcfc2cfc4cf43c700df6afb6b7c13
Aneurinmind <50070328+Aneurinmind@users.noreply.github.com>
2020-09-17 08:41:25 +0800

    Add files via upload

--- js/poem_6.js
+++ js/poem_6.js
@@ -8,3 +8,15 @@ function StopSound(bgm_1) {
     thissound.pause();
     thissound.currentTime = 0;
 }
+
+var isPlaying =false; 
+	function readPlay(bgm_2){
+	var player = document.getElementById('bgm_2');
+	if (isPlaying) {
+	// 如果正在播放, 停止播放并停止读取此音乐文件
+	    player.pause();  
+	} else {
+	    player.play();
+	}
+	isPlaying = !isPlaying;
+	}
